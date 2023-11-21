d6dafdbe2ead40988e4484a91e244b8772712683
mfontosco <59096300+mfontosco@users.noreply.github.com>
2023-11-21 14:39:29 +0100

    just added some icons on home page

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,16 +5,16 @@ const Dropdown = () => {
   return (
     <div className='min-h-screen bg-black text-white'>
        <div className="md:hidden flex items-center flex-col justify-center gap-6">
-            <Link to="" className=" mt-6 uppercase text-white border border-b-1 block font-semibold">
+            <Link to="" className=" mt-6 uppercase text-white block font-semibold">
               About
             </Link>
-            <Link to="" className="uppercase text-white border border-b-1 block font-semibold">
+            <Link to="" className="uppercase text-white block font-semibold">
               Services
             </Link>
-            <Link to="" className="uppercase text-white border border-b-1 block font-semibold">
+            <Link to="" className="uppercase text-white block font-semibold">
               Portfolio
             </Link>
-            <Link to="" className="uppercase text-white border border-b-1 block font-semibold">
+            <Link to="" className="uppercase text-white block font-semibold">
               Contact
             </Link>
           </div>
